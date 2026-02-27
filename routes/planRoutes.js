98e828aa8e1618c7cac21c07efa640a8cb17e0c3
Aditya Verma <109659297+Adityaverma6860@users.noreply.github.com>
2026-02-27 16:40:38 +0530

    Update planRoutes.js

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 const { authMiddleware, adminMiddleware } = require("../middleware/jwt");
-const { createPlan, publishPlan, getPublishedPlans } = require("../controllers/planController");
+const { createPlan,updatePlan, getPublishedPlans } = require("../controllers/planController");
 
 router.post("/", authMiddleware, adminMiddleware, createPlan);
-router.patch("/:id/", authMiddleware, adminMiddleware, publishPlan);
+router.patch("/:id/", authMiddleware, adminMiddleware, updatePlan);
 router.get("/", getPublishedPlans);
 
 module.exports = router;
 
 
 // http://localhost:5000/api/plans
-// http://localhost:5000/api/plans/:id/
+// http://localhost:5000/api/update/:id/
+
+// {
+//   "name": "Premium Plan Plus",
+//   "description": "Access all premium features for 2 months.",
+//   "price": 899,
+//   "duration": 60,
+//   "isPublished": true
+// }
+
 //  http://localhost:5000/api/plans   
